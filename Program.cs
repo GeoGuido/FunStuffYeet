115d48fca6bf58ee0bd61b25d6cc29c38fc825ef
GuidoCraftGamer <48997111+GuidoCraftGamer@users.noreply.github.com>
2019-03-28 16:30:22 +0100

    The actual CODE!

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FunStuffYeet
+{
+    class Program
+    {
+        static void Main()
+        {
+            for (int q = 1; q < 10; q++)
+            {
+                for (int i = 1; i < 40; i++)
+                {
+                    Console.SetWindowSize(i, i);
+                    System.Threading.Thread.Sleep(50);
+                }
+            }
+        }
+    }
+}
